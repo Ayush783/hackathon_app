ac168c69e448a63b1fc9cc6938ea62de89b2a745
Ayush783 <ayushc45xxx@gmail.com>
2020-03-01 03:53:14 +0530

    third commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:hackathon_app/location.dart';
-import 'package:hackathon_app/resData.dart';
+import 'package:http/http.dart';
+
+const apikey1 = "ba4fa1af6358a8acb8783da428c5e77d";
 
 class Homepage extends StatefulWidget {
   @override
@@ -15,7 +19,7 @@ class _HomepageState extends State<Homepage> {
   void initState() {
     super.initState();
     getLocation();
-    getRestaurantData();
+    resData();
   }
 
   void getLocation() async {
@@ -25,9 +29,87 @@ class _HomepageState extends State<Homepage> {
     lon = location.longitude;
   }
 
-  void getRestaurantData() async {
-    ResData resData = ResData();
-    await resData.resData();
+  var m;
+  String name1;
+  String name2;
+  String name3;
+  String name4;
+  String name5;
+  String name6;
+  String name7;
+  String name8;
+  String name9;
+  String rat1;
+  String rat2;
+  String rat3;
+  String rat4;
+//int rat5;
+  String rat6;
+  String rat7;
+  String rat8;
+  String rat9;
+  Future<void> resData() async {
+    Response response = await get(
+        'https://developers.zomato.com/api/v2.1/location_details?entity_id=11882&entity_type=city',
+        headers: {
+          'content-type': 'application/json',
+          'user-key': apikey1,
+          "access-control-allow-credentials": "true"
+        });
+    m = response.body;
+    String _name1 =
+        await jsonDecode(m)['best_rated_restaurant'][0]['restaurant']['name'];
+    name1 = _name1;
+    String _name2 =
+        await jsonDecode(m)['best_rated_restaurant'][1]['restaurant']['name'];
+    String _name3 =
+        await jsonDecode(m)['best_rated_restaurant'][2]['restaurant']['name'];
+    String _name4 =
+        await jsonDecode(m)['best_rated_restaurant'][3]['restaurant']['name'];
+    String _name5 =
+        await jsonDecode(m)['best_rated_restaurant'][4]['restaurant']['name'];
+    String _name6 =
+        await jsonDecode(m)['best_rated_restaurant'][5]['restaurant']['name'];
+    String _name7 =
+        await jsonDecode(m)['best_rated_restaurant'][6]['restaurant']['name'];
+    String _name8 =
+        await jsonDecode(m)['best_rated_restaurant'][7]['restaurant']['name'];
+    String _name9 =
+        await jsonDecode(m)['best_rated_restaurant'][8]['restaurant']['name'];
+    name2 = _name2;
+    name3 = _name3;
+    name4 = _name4;
+    name5 = _name5;
+    name6 = _name6;
+    name7 = _name7;
+    name8 = _name8;
+    name9 = _name9;
+    String _rat1 = await jsonDecode(m)['best_rated_restaurant'][0]['restaurant']
+        ['user_rating']['aggregate_rating'];
+    String _rat2 = await jsonDecode(m)['best_rated_restaurant'][1]['restaurant']
+        ['user_rating']['aggregate_rating'];
+    String _rat3 = await jsonDecode(m)['best_rated_restaurant'][2]['restaurant']
+        ['user_rating']['aggregate_rating'];
+    String _rat4 = await jsonDecode(m)['best_rated_restaurant'][3]['restaurant']
+        ['user_rating']['aggregate_rating'];
+    //int _rat5=await jsonDecode(m)['best_rated_restaurant'][4]['restaurant']['user_rating']['aggregate_rating'];
+    String _rat6 = await jsonDecode(m)['best_rated_restaurant'][5]['restaurant']
+        ['user_rating']['aggregate_rating'];
+    String _rat7 = await jsonDecode(m)['best_rated_restaurant'][6]['restaurant']
+        ['user_rating']['aggregate_rating'];
+    String _rat8 = await jsonDecode(m)['best_rated_restaurant'][7]['restaurant']
+        ['user_rating']['aggregate_rating'];
+    String _rat9 = await jsonDecode(m)['best_rated_restaurant'][8]['restaurant']
+        ['user_rating']['aggregate_rating'];
+    rat1 = _rat1;
+    rat2 = _rat2;
+    rat3 = _rat3;
+    rat4 = _rat4;
+    //rat5=_rat5;
+    rat6 = _rat6;
+    rat7 = _rat7;
+    rat8 = _rat8;
+    rat9 = _rat9;
   }
 
   @override
@@ -65,16 +147,81 @@ class _HomepageState extends State<Homepage> {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Rescard(ResData().data(0), 'address', 'rating',
-                  'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
-              Rescard(ResData().data(1).toString(),'ad2','rat2','https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665__340.jpg'),
-              Rescard(ResData().data(2).toString(),'ad2','rat2','https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552__340.jpg'),
-              Rescard(ResData().data(3).toString(),'ad2','rat2','https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246__340.jpg'),
-              Rescard(ResData().data(4).toString(),'ad2','rat2','https://image.shutterstock.com/image-photo/chicken-skewers-slices-apples-chili-260nw-426536971.jpg'),
-              Rescard(ResData().data(5).toString(),'ad2','rat2','https://cdn.pixabay.com/photo/2017/06/16/07/00/breakfast-2408035__340.jpg'),
-              Rescard(ResData().data(6).toString(),'ad2','rat2','https://cdn.pixabay.com/photo/2018/05/17/17/07/fried-3409076__340.jpg'),
-              Rescard(ResData().data(7).toString(),'ad2','rat2','https://cdn.pixabay.com/photo/2020/02/11/15/41/shrimp-4839919__340.jpg'),
-              Rescard(ResData().data(8).toString(),'ad2','rat2','https://cdn.pixabay.com/photo/2014/11/03/23/33/food-516044__340.jpg'),
+              RawMaterialButton(
+                splashColor: Colors.transparent,
+                onPressed: () {
+                  Navigator.pushNamed(context, '5');
+                },
+                child: Rescard(name1.toString(), 'Muradnagar', rat1.toString(),
+                    'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+              ),
+              RawMaterialButton(
+                splashColor: Colors.transparent,
+                onPressed: () {
+                  Navigator.pushNamed(context, '6');
+                },
+                child: Rescard(name2.toString(), 'Muradnagar', rat2.toString(),
+                    'https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665__340.jpg'),
+              ),
+              RawMaterialButton(
+                splashColor: Colors.transparent,
+                onPressed: () {
+                  Navigator.pushNamed(context, '7');
+                },
+                child: Rescard(name3.toString(), 'Muradnagar', rat3.toString(),
+                    'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552__340.jpg'),
+              ),
+              RawMaterialButton(
+                splashColor: Colors.transparent,
+                onPressed: () {
+                  Navigator.pushNamed(context, '8');
+                },
+                child: Rescard(name4.toString(), 'Muradnagar', rat4.toString(),
+                    'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246__340.jpg'),
+              ),
+              RawMaterialButton(
+                splashColor: Colors.transparent,
+                onPressed: () {
+                  Navigator.pushNamed(context, '9');
+                },
+                child: Rescard(name5.toString(), 'Muradnagar', '0',
+                    'https://image.shutterstock.com/image-photo/chicken-skewers-slices-apples-chili-260nw-426536971.jpg'),
+              ),
+              RawMaterialButton(
+                splashColor: Colors.transparent,
+                onPressed: () {
+                  Navigator.pushNamed(context, '10');
+                },
+                child: Rescard(name6.toString(), 'Muradnagar', rat6.toString(),
+                    'https://cdn.pixabay.com/photo/2017/06/16/07/00/breakfast-2408035__340.jpg'),
+              ),
+              RawMaterialButton(
+                splashColor: Colors.transparent,
+                onPressed: () {
+                  Navigator.pushNamed(context, '11');
+                },
+                child: Rescard(
+                    'Up 14 food point',
+                    'Muradnagar',
+                    rat7.toString(),
+                    'https://cdn.pixabay.com/photo/2018/05/17/17/07/fried-3409076__340.jpg'),
+              ),
+              RawMaterialButton(
+                splashColor: Colors.transparent,
+                onPressed: () {
+                  Navigator.pushNamed(context, '12');
+                },
+                child: Rescard(name8.toString(), 'Muradnagar', rat8.toString(),
+                    'https://cdn.pixabay.com/photo/2020/02/11/15/41/shrimp-4839919__340.jpg'),
+              ),
+              RawMaterialButton(
+                splashColor: Colors.transparent,
+                onPressed: () {
+                  Navigator.pushNamed(context, '13');
+                },
+                child: Rescard(name9.toString(), 'Muradnagar', rat9.toString(),
+                    'https://cdn.pixabay.com/photo/2014/11/03/23/33/food-516044__340.jpg'),
+              ),
             ],
           )
         ],
@@ -99,57 +246,83 @@ class Rescard extends StatefulWidget {
 class _RescardState extends State<Rescard> {
   @override
   Widget build(BuildContext context) {
-    return RawMaterialButton(
-      splashColor: Colors.transparent,
-      onPressed: (){
-        Navigator.pushNamed(context, '5');
-      },
-          child: Card(
-        elevation: 20,
-        child: Row(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(right: 30),
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
-              height: 110,
-              width: 120,
-              child: Image(
-                image: NetworkImage(widget.url),
-                fit: BoxFit.fill,
-              ),
+    return Card(
+      elevation: 20,
+      child: Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 20),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+            height: 110,
+            width: 120,
+            child: Image(
+              image: NetworkImage(widget.url),
+              fit: BoxFit.fill,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Icon(Icons.restaurant,size: 16,),
-                    Padding(padding: EdgeInsets.only(right:5),),
-                    Text(widget.name,style: TextStyle(fontSize: 10),),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Icon(Icons.home,size: 16,),
-                    Padding(padding: EdgeInsets.only(right:5),),
-                    Text(widget.address),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Icon(Icons.star,size: 10,),
-                    Icon(Icons.star,size: 10,),
-                    Icon(Icons.star,size: 10,),
-                    Icon(Icons.star,size: 10,),
-                    Icon(Icons.star_border,size: 10,),
-                    Padding(padding: EdgeInsets.only(right:5),),
-                    Text(widget.rating,),
-                  ],
-                ),
-              ],
-            )
-          ],
-        ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.restaurant,
+                    size: 16,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 5),
+                  ),
+                  Text(
+                    widget.name,
+                    style: TextStyle(fontSize: 10),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.home,
+                    size: 16,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 5),
+                  ),
+                  Text(widget.address),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.star,
+                    size: 10,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 10,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 10,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 10,
+                  ),
+                  Icon(
+                    Icons.star_border,
+                    size: 10,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 5),
+                  ),
+                  Text(
+                    widget.rating,
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
--- lib/restaurant_page.dart
+++ lib/restaurant_page.dart
@@ -1,11 +1,111 @@
 import 'package:flutter/material.dart';
 
-class ResPage extends StatefulWidget {
+class ResPage1 extends StatefulWidget {
   @override
-  _ResPageState createState() => _ResPageState();
+  _ResPage1State createState() => _ResPage1State();
 }
 
-class _ResPageState extends State<ResPage> {
+class _ResPage1State extends State<ResPage1> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+                  child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                height: 200,
+                width: double.infinity,
+                decoration: (BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: NetworkImage(
+                            'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40),
+                    ),
+                    color: Colors.orange)),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  "Naaz Chicken",
+                  style: TextStyle(
+                    fontSize: 27,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text.rich(
+                  TextSpan(
+                    text: 'Address :',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text:
+                              '   OBC bank bus stand, Muradnagar Locality, Muradnagar ',
+                          style: TextStyle(
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.normal)),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 25),
+                width: 300,
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Colors.grey),
+                  ),
+                ),
+                child: Center(
+                  child: Text(
+                    "Restaurant  Menu",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Dishes('Chaap Afghani',190.00),
+              Dishes('Tandoori Fried Chicken',120.0),
+              Dishes('Butter Tandoori Chicken',120.0),
+              Dishes('Chicken Kebab',100.0),
+              Dishes('Chicken Changezi',160.0),
+              Dishes('Shahi Paneer',150.0),
+              Dishes('Butter Chicken',120.0),
+              Dishes('Chicken Kaali Mirch',120.0),
+              Dishes('Chicken Tandoori Nice',260.0),
+              Dishes('Chicken Afghani',150.0),
+              Dishes('Chicken tikka',119.0),
+              Dishes('paneer Tikka',250.0),
+              Dishes('Fried chicken',190.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ResPage2 extends StatefulWidget {
+  @override
+  _ResPage2State createState() => _ResPage2State();
+}
+
+class _ResPage2State extends State<ResPage2> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -15,9 +115,9 @@ class _ResPageState extends State<ResPage> {
           width: double.infinity,
           decoration: (BoxDecoration(
               image: DecorationImage(
-                fit: BoxFit.fill,
+                  fit: BoxFit.fill,
                   image: NetworkImage(
-                      'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')),
+                      'https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665__340.jpg')),
               borderRadius: BorderRadius.only(
                 bottomLeft: Radius.circular(40),
                 bottomRight: Radius.circular(40),
@@ -26,5 +126,266 @@ class _ResPageState extends State<ResPage> {
         ),
       ),
     );
+  }
+}
+
+class ResPage3 extends StatefulWidget {
+  @override
+  _ResPage3State createState() => _ResPage3State();
+}
+
+class _ResPage3State extends State<ResPage3> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: 200,
+          width: double.infinity,
+          decoration: (BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                      'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552__340.jpg')),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+              color: Colors.orange)),
+        ),
+      ),
+    );
+  }
+}
+
+class ResPage4 extends StatefulWidget {
+  @override
+  _ResPage4State createState() => _ResPage4State();
+}
+
+class _ResPage4State extends State<ResPage4> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: 200,
+          width: double.infinity,
+          decoration: (BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                      'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246__340.jpg')),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+              color: Colors.orange)),
+        ),
+      ),
+    );
+  }
+}
+
+class ResPage5 extends StatefulWidget {
+  @override
+  _ResPage5State createState() => _ResPage5State();
+}
+
+class _ResPage5State extends State<ResPage5> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: 200,
+          width: double.infinity,
+          decoration: (BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                      'https://image.shutterstock.com/image-photo/chicken-skewers-slices-apples-chili-260nw-426536971.jpg')),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+              color: Colors.orange)),
+        ),
+      ),
+    );
+  }
+}
+
+class ResPage6 extends StatefulWidget {
+  @override
+  _ResPage6State createState() => _ResPage6State();
+}
+
+class _ResPage6State extends State<ResPage6> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: 200,
+          width: double.infinity,
+          decoration: (BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                      'https://cdn.pixabay.com/photo/2017/06/16/07/00/breakfast-2408035__340.jpg')),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+              color: Colors.orange)),
+        ),
+      ),
+    );
+  }
+}
+
+class ResPage7 extends StatefulWidget {
+  @override
+  _ResPage7State createState() => _ResPage7State();
+}
+
+class _ResPage7State extends State<ResPage7> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: 200,
+          width: double.infinity,
+          decoration: (BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                      'https://cdn.pixabay.com/photo/2018/05/17/17/07/fried-3409076__340.jpg')),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+              color: Colors.orange)),
+        ),
+      ),
+    );
+  }
+}
+
+class ResPage8 extends StatefulWidget {
+  @override
+  _ResPage8State createState() => _ResPage8State();
+}
+
+class _ResPage8State extends State<ResPage8> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: 200,
+          width: double.infinity,
+          decoration: (BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                      'https://cdn.pixabay.com/photo/2020/02/11/15/41/shrimp-4839919__340.jpg')),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+              color: Colors.orange)),
+        ),
+      ),
+    );
+  }
+}
+
+class ResPage9 extends StatefulWidget {
+  @override
+  _ResPage9State createState() => _ResPage9State();
+}
+
+class _ResPage9State extends State<ResPage9> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: 200,
+          width: double.infinity,
+          decoration: (BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                      'https://cdn.pixabay.com/photo/2014/11/03/23/33/food-516044__340.jpg')),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+              color: Colors.orange)),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class Dishes extends StatefulWidget {
+
+  final String dishname;
+  final double price;
+  Dishes(this.dishname, this.price);
+  @override
+  _DishesState createState() => _DishesState();
+}
+
+class _DishesState extends State<Dishes> {
+  int i=0;
+  @override
+  Widget build(BuildContext context) {
+   return Card(
+        child: Row(children: <Widget>[
+          Padding(padding: EdgeInsets.only(left:30),),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+            Container(
+              width: 110,
+              child: Text(widget.dishname,style: TextStyle(fontSize: 12),)),
+            Text("Rs.${widget.price}",style: TextStyle(fontSize: 8),)
+          ],),
+          Padding(padding: EdgeInsets.only(right:100),),
+          Container(
+            
+            child: Row(children: <Widget>[
+        RawMaterialButton(
+          constraints: BoxConstraints(minHeight: 10,minWidth: 10),
+          onPressed: (){
+          setState(() {
+            if(i>0){
+              i--;
+            }
+          });
+      },
+      child: Icon(Icons.remove),
+      ),
+      Text("$i"),
+      RawMaterialButton(
+        constraints: BoxConstraints(minHeight: 10),
+        onPressed: (){
+          setState(() {
+            i++;
+          });
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Icon(Icons.add),
+      ),
+      ],),)
+        ],)
+        );
   }
 }
